676b7d9687c21e2889cf4d284619e8a8c23bba88
Yassine Dardour <dardour.yassine@gmail.com>
2023-07-12 09:59:36 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('>Yo!');
     res.end();
-}).listen(process.env.PORT || 3000);
+});
+const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:8080"
+    }
+  });
+  
+  httpServer.listen(process.env.PORT || 3000);
+
+io.on("connection", (socket) => {
+  console.log("connetion now");
+
+
+ setTimeout(() => {
+  socket.emit('gameStatus', {gameStatus:100});
+ }, 10000);
+
+
+
+});
